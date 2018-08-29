b3e38de034ad96a8711e9d61971fb48f57681991
NiklasWilke <NiklasWilke@users.noreply.github.com>
2018-08-29 16:59:09 +0200

    Commit

--- public/scripts/drawing_functions.js
+++ public/scripts/drawing_functions.js
@@ -271,11 +271,25 @@ CanvasRenderingContext2D.prototype.drawTank = function(tank)
 	
 	
 	// pipe
-	this.beginPath();
-	this.roundRect(h*0.3/2, -w*0.2/2, h*0.45, w*0.2, w*0.05);
-	this.fillStyle = "hsl("+tank.color.h+", "+tank.color.s+"%, "+(tank.color.l*0.7)+"%)";
-	this.fill();
-	this.closePath();
+	switch (tank.weapontype)
+	{
+		case "test":
+			this.beginPath();
+			this.roundRect(h*0.3/2, -w*0.2/2, h*0.45, w*0.2, w*0.05);
+			this.fillStyle = "hsl(360,100%,50%)";
+			this.fill();
+			this.closePath();
+			break;
+		default:
+		case "default":
+			this.beginPath();
+			this.roundRect(h*0.3/2, -w*0.2/2, h*0.45, w*0.2, w*0.05);
+			this.fillStyle = "hsl("+tank.color.h+", "+tank.color.s+"%, "+(tank.color.l*0.7)+"%)";
+			this.fill();
+			this.closePath();
+			break
+	}
+	
 	
 	// head
 	this.beginPath();
